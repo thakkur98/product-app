117535a14fee354d66522886f5ade20575ecdb9e
thakkur98 <88381511+thakkur98@users.noreply.github.com>
2025-10-29 10:39:37 +0530

    Update index.tsx

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -306,7 +306,7 @@ export const DashboardStyle = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    z-index: 1000;
+    z-index: 2;
 
     .cart-content {
       background: #fff;
